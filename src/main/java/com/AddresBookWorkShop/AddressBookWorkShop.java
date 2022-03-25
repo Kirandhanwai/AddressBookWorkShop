cdc827079536b4a58c941fafb05b7c92c54cdbc3
Kirandhanwai <kirandhanwai123@gmail.com>
2022-03-25 09:30:40 +0530

    Added = Welcome to AddressBook program

---
+++ src/main/java/com/AddresBookWorkShop/AddressBookWorkShop.java
@@ -0,0 +1,7 @@
+package com.AddresBookWorkShop;
+
+public class AddressBookWorkShop {
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBookWorkShop Program");
+	}
+}
